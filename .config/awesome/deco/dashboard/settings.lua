1e037717029602f2a7f287389cadf74cfff886d2
Sephrost <lorenzatofabi13@gmail.com>
2023-08-04 16:51:55 +0200

    Made adjustments to settings

--- .config/awesome/deco/dashboard/settings.lua
+++ .config/awesome/deco/dashboard/settings.lua
@@ -4,15 +4,17 @@ local beautiful = require("beautiful")
 local gears = require("gears")
 local dpi = require("beautiful.xresources").apply_dpi
 
-local function makeButton(icon)
+local function makeButton(icon,color)
+    local textbox = wibox.widget {
+      widget = wibox.widget.textbox,
+      text = icon,
+      font = "SauceCodePro 10",
+      align = "center",
+    }
+    
     local button = wibox.widget {
       {
-        {
-          widget = wibox.widget.textbox,
-          text = icon,
-          font = "SauceCodePro 10",
-          align = "center",
-        },
+        textbox,
         margins = dpi(10),
         widget = wibox.container.margin
       },
@@ -27,26 +29,30 @@ local function makeButton(icon)
     
     local function toggle_color()
       if button.selected then
-        button.bg = beautiful.palette.mantle
+        -- color text 
+        textbox.markup = "<span foreground='" .. beautiful.palette.text .. "'>" .. icon .. "</span>"
         button.selected = false
       else
-        button.bg = beautiful.bg_focus
+        -- color text
+        textbox.markup = "<span foreground='" .. color .. "'>" .. icon .. "</span>"
         button.selected = true
       end
     end
 
-    button:connect_signal("button::press", function()
-      toggle_color()
+    button:connect_signal("button::press", function(_,_,_,button)
+      if button == 1 or button == 3 then
+        toggle_color()
+      end
     end)
 
     return button
 end
 
-local wifi_button = makeButton("")
-local bluetooth_button = makeButton("")
-local battery_button = makeButton("󱟦")
+local wifi_button = makeButton("",beautiful.palette.blue)
+local bluetooth_button = makeButton("",beautiful.palette.mauve)
+local battery_button = makeButton("󱟦",beautiful.palette.green)
 local volume_button = makeButton("")
-local airplane_button = makeButton("󰀝")
+local airplane_button = makeButton("󰀝",beautiful.palette.flamingo)
 
 return {
   wifi = wifi_button,
