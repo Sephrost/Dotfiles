ad094397cd44b0d7eacc1b5f9382742d18d1dfa5
Sephrost <lorenzatofabi13@gmail.com>
2023-08-06 11:40:01 +0200

    Bugfix

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -73,8 +73,14 @@ function _M.get(clientkeys, clientbuttons)
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox" },
+      properties = { 
+        -- screen = 1, 
+        -- tag = "2" 
+        -- floating = false,
+        maximized = false,
+      } 
+    },
 
   }
 
