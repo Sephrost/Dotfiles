0e2005b18ff175b089a9b0f392d3e090df2132a3
Sephrost <lorenzatofabi13@gmail.com>
2023-08-15 11:07:38 +0200

    Fixed tooltip text

--- .config/awesome/deco/wibar/widgets/battery.lua
+++ .config/awesome/deco/wibar/widgets/battery.lua
@@ -24,11 +24,12 @@ local function update_battery_status(fun)
         awful.spawn.easy_async_with_shell(
           "cat /sys/class/power_supply/BAT" .. tostring(i - 1) .. "/status",
           function(stdout)
-            battery_status[i].status = stdout
+            battery_status[i].status = string.gsub(stdout, "\n", "")
             awful.spawn.easy_async_with_shell(
             "cat /sys/class/power_supply/BAT" .. tostring(i - 1) .. "/capacity",
             function(stdout)
-              battery_status[i].percentage = stdout
+              --stip new line 
+              battery_status[i].percentage = string.gsub(stdout, "\n", "")
               total_battery_percentage = total_battery_percentage + tonumber(stdout)
               if fun ~= nil then
                 fun()
@@ -45,7 +46,7 @@ end
 local function create_battery_widget()
   local battery_progressbar = wibox.widget {
     max_value = 100,
-    value = 50,
+    value = 100,
     forced_height = dpi(1),
     forced_width = 50,
     border_width = 1,
@@ -58,24 +59,14 @@ local function create_battery_widget()
     widget = wibox.widget.progressbar
   }
 
-  local bat_percentage_value = 50
-
-  local battery_percentage_text = wibox.widget {
-    text = tostring(bat_percentage_value) .. "%",
-    font = "SauceCodePro 8",
-    id = "percentage_text",
-    align = "center",
-    valign = "center",
-    widget = wibox.widget.textbox
-  }
-
   local battery_tooltip = awful.tooltip{
     objects = {battery_progressbar},
     timeout = 10,
     timer_function = function()
       local str = ""
       for i = 1, battery_number do 
-        str = str .. "Battery " .. tostring(i) .. ": " .. battery_status[i].percentage .. "% " .. battery_status[i].status .. "\n"
+        str = str .. "Battery " .. tostring(i) .. ": " .. battery_status[i].percentage .. "% " 
+          .. battery_status[i].status .. "\n" .. (i==battery_number and "" or "\n")
       end
 
       return str
@@ -85,7 +76,6 @@ local function create_battery_widget()
   local function update_progressbar()
     local val = total_battery_percentage / battery_number
     battery_progressbar.value = val
-    battery_percentage_text.text = tostring(val) .. "%"
 
     local is_charging = false
     for i = 1, battery_number do
@@ -96,7 +86,6 @@ local function create_battery_widget()
 
     if val < 20 then
       battery_progressbar.color = battery_low_color
-    -- if it is charging 
     elseif is_charging then
       battery_progressbar.color = beautiful.palette.mauve
     else
